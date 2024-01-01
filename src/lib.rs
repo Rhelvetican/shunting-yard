8d0bf69264cf94d9fa48cee57cc4e495807aed39
Rh4096 <bionicvnb@gmail.com>
2024-01-01 21:38:30 +0700

    Add evaluate function for code evaluation

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,16 @@ extern crate num;
 pub mod token;
 pub mod parser;
 pub mod eval;
+
+use eval::eval;
+use num::Num;
+use parser::parse;
+use std::str::FromStr;
+
+pub fn evaluate<T>(code: &str) -> Result<T, String> where T: Num + FromStr + Clone + Copy + Into<f64>
+{
+    match parse::<T>(code) {
+        Ok(tokens) => Ok(eval(&tokens)),
+        Err(e) => Err(e),
+    }
+}
