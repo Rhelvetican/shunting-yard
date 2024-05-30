e0e34fd91dff886fe649440230909385fca2ace8
Rh4096 <rhelvetican@gmail.com>
2024-05-31 00:28:18 +0700

    chore: Refactor main.rs to use evaluate function for better code organization

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -29,7 +29,7 @@ fn repl() -> Result<()> {
         if buffer.trim() == "exit" {
             break;
         }
-        let v = shunting_yard::evaluate::<f64>(&buffer)?;
+        let v = evaluate::<f64>(&buffer)?;
         buffer.clear();
         println!(">>> {}", v);
     }
